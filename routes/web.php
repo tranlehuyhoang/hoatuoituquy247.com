398b7ae5ce1479848a941d8888976ef7292ef576
2509roblox <2509roblox@gmail.com>
2024-11-30 10:11:39 +0700

    ádfafsdfasdasfd

--- routes/web.php
+++ routes/web.php
@@ -71,69 +71,24 @@ Route::get('/categories/{slug}', NewCategory::class);
 Route::get('/product/{slug}', NewProductDetail::class);
 Route::get('/gio-hang', NewCart::class);
 Route::get('/thanh-toan', NewCheckout::class);
-Route::get('/don-hang-da-nhan/cam-on-quy-khach', NewPayment::class);
+// Route::get('/don-hang-da-nhan/cam-on-quy-khach', NewPayment::class);
 Route::get('/gioi-thieu', NewGioiThieu ::class);
 Route::get('/lien-he', NewLienHe::class);
-Route::get('/thac-mac-khi-dat-hoa', NewThacMacKhiDatHoa::class);
+// Route::get('/thac-mac-khi-dat-hoa', NewThacMacKhiDatHoa::class);
 Route::get('/chinh-sach-bao-mat', NewChinhSachBaoMat::class);
 Route::get('/chinh-sach-doi-tra', NewChinhSachDoiTra::class);
-Route::get('/chi-nhanh', NewChiNhanh::class);
+// Route::get('/chi-nhanh', NewChiNhanh::class);
 Route::get('/huong-dan-dat-hoa', NewHuongDanDatHoa::class);
 Route::get('/huong-dan-thanh-toan', NewHuongDanThanhToan::class);
 Route::get('/quy-dinh-van-chuyen', NewQuyDinhVanChuyen::class);
-Route::get('/y-nghia-hoa', NewYNghiaHoa::class);
-Route::get('/doi-tac-tieu-bieu', NewDoiTacTieuBieu::class);
-Route::get('/danh-gia-hinh-anh-thuc-te', NewDanhGiaHinhAnhThucTe::class);
-Route::get('/khuyen-mai', NewKhuyenMai::class);
-Route::get('/cay-lan-chi', NewCayLanChi::class);
+// Route::get('/y-nghia-hoa', NewYNghiaHoa::class);
+// Route::get('/doi-tac-tieu-bieu', NewDoiTacTieuBieu::class);
+// Route::get('/danh-gia-hinh-anh-thuc-te', NewDanhGiaHinhAnhThucTe::class);
+// Route::get('/khuyen-mai', NewKhuyenMai::class);
+// Route::get('/cay-lan-chi', NewCayLanChi::class);
 Route::get('/cua-hang', NewCuaHang::class);
 Route::get('/thanks/{code}', NewThanks::class);
 Route::get('/checkpayment/{orderCode}', [CheckPaymentController::class, 'checkPayment']);
 
 
-
-
-// Route::get('/about', About::class);
-// Route::get('/contact', Contact::class);
-// Route::get('/chinh-sach-van-chuyen', ShippingPolicy::class);
-// Route::get('/chinh-sach-bao-mat', PrivacyPolicy::class);
-// Route::get('/chinh-sach-thanh-toan', PaymentPolicy::class);
-// Route::get('/chinh-sach-bao-hanh', WarrantyPolicy::class);
-// Route::get('/chinh-sach-doi-tra-hang-hoan-tien', ReturnPolicy::class);
-// Route::get('/apps/kiem-tra-don-hang', OrderTracking::class);
-// Route::get('/blogs', Blog::class);
-// Route::get('/blog/{slug}', BlogPost::class);
-
-
-// Account routes
-
-// Route::middleware([CheckLogin::class])->group(function () {
-//     Route::get('/account', Account::class);
-//     Route::get('/account/orders', AccountOrders::class);
-//     Route::get('/account/orders/{orderCode}', AccountOrdersDetail::class);
-//     Route::get('/account/changepassword', ChangePassword::class);
-//     // Route::get('/account/addresses', AccountAddresses::class);
-//     Route::get('/account/logout', Logout::class);
-//     Route::get('/checkout', Checkout::class);
-//     Route::get('/thanks/{orderCode}', Thankyou::class)->name('thanks');
-//     Route::get('/bank/{orderCode}', Bank::class)->name('bank');
-// });
-
-
-
-
-// Route::get('/shop', Shop::class);
-// Route::get('/product/{slug}', ProductDetail::class);
-// Route::get('/product1/{slug}', ProductDetail1::class);
-
-
-
-// Route::middleware(RedirectIfAuthenticated::class)->group(function () {
-//     Route::get('/auth/google', [Login::class, 'redirectToProvider'])->name('google.login');
-//     Route::get('/auth/google/register', [Register::class, 'redirectToProvider'])->name('google.register');
-//     Route::get('/auth/google/callback', [Login::class, 'handleGoogleCallback']);
-//     Route::get('/account/login', Login::class);
-//     Route::get('/account/register', Register::class);
-// });
-// Route::get('/checkpayment/{orderCode}', [CheckPaymentController::class, 'checkPayment']);
 Route::get('/transactions', Transactions::class);
